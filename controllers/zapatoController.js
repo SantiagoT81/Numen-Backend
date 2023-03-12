74d4c93745f9d5d8127368ccc7773eeeeef9b3cc
SantiagoT81 <santytouris91@gmail.com>
2023-03-12 04:25:07 -0300

    ruta 3 agregada

--- controllers/zapatoController.js
+++ controllers/zapatoController.js
@@ -23,4 +23,37 @@ const getZapatosSizes = async (req, res) => {
     }
 }
 
-module.exports = {getZapatos, getZapatosSizes}
+const postZapatos = async (req,res) => {
+    try{
+        const validationError = validationResult(req);
+        if (validationError.isEmpty()){
+            const zapato = new Zapato(req.body);
+            await zapato.save();
+            res.status(201).send('Zapatos registrados exitosamente')
+        }else{
+            res.status(400).json({msg:"Error al registrar entrada ", error: validationError.errors })
+        }
+    }catch(error){
+        res.status(500).json({errores:error})
+    }
+};
+
+const deleteZapatos = async(req,res) => {
+    try {
+        await Zapato.findByIdAndDelete(req.params.id)
+        res.status(201).send('Registro eliminado exitosamente')
+    }catch{
+        res.status(500).send('Ocurrió un error al eliminar el registro')
+    }
+};
+
+const updateZapatos = async(req, res) => {
+    try {
+        await Zapato.findByIdAndUpdate(req.params.id, req.body)
+        res.status(201).send('Registro actualizado exitosamente')
+    }catch{
+        res.status(500).send('Ocurrió un error al actualizar el registro')
+    }
+};
+
+module.exports = {getZapatos, getZapatosSizes, postZapatos, deleteZapatos, updateZapatos}
--- routes/zapatos.js
+++ routes/zapatos.js
@@ -6,4 +6,14 @@ const zapatoController = require('../controllers/zapatoController')
 router.get("/", zapatoController.getZapatos)
 router.get("/talles/:size", zapatoController.getZapatosSizes)
 
+router.post("/ingresar", 
+        [
+            check("model").notEmpty().withMessage("Modelo requerido").not().isNumeric().withMessage('El nombre del modelo solo contiene números'),
+            check("size").isInt({min:1, max:46}).withMessage("El talle tiene un valor negativo o mayor al aceptado (46)").notEmpty().withMessage('Talle requerido')
+        ],zapatoController.postZapatos)
+
+router.delete("/borrar/:id", zapatoController.deleteZapatos)
+
+router.put("/actualizar/:id",zapatoController.updateZapatos)
+
 module.exports = router
